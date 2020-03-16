d2250adf0727cc789085f988103dce93c78b6279
dudamaciej88 <dudamaciej88@gmail.com>
2020-03-16 17:36:59 +0100

    Added homepage to package.json

---
+++ build/precache-manifest.2dafa1d5a42c4289702e2db23ed68d8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc0dfed5875d3a5b46e29db967e3172",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e53779443b05751ebc8",
+    "url": "./static/css/main.c159cba2.chunk.css"
+  },
+  {
+    "revision": "35a666d72429f192c112",
+    "url": "./static/js/2.db62fd76.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.db62fd76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e53779443b05751ebc8",
+    "url": "./static/js/main.f2de5ba8.chunk.js"
+  },
+  {
+    "revision": "926222d5c6c6092992d2",
+    "url": "./static/js/runtime-main.944adb4e.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3ae935d3fd7f58d335ed2b1e0276e6c.js"
+  "./precache-manifest.2dafa1d5a42c4289702e2db23ed68d8c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
